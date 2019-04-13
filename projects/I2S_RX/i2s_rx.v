6e40f6fbb99f0faf101417a7d0d8e46432c24643
goran.mahovlic <goran.mahovlic@gmail.com>
2019-04-13 21:34:10 +0200

    buffer still not working

--- projects/I2S_RX/i2s_rx.v
+++ projects/I2S_RX/i2s_rx.v
@@ -3,46 +3,30 @@ module i2s_mic
 	parameter size		= 32
 )
 (
-    input wire standard_clk, // 2MHz (standard mode)
-	input wire ultrasonic_clk, // 4MHz (ultrasonic mode)
-		input wire [6:0] btn,
-		input wire data_in,  // data from microphone
-		output wire mic_clk_out,
-		output wire data_ready,
+    input wire clk, // 2MHz (standard mode)
+	input wire data_in,  // data from microphone
+	output wire mic_clk_out,
+	output wire data_ready,
     output reg [size-1:0] data_out // data stored into buffer
     );
 
-wire audio_clk;
-
-// currently used instead of switch case statement
-// if button is hold ultrasonic clock will be send to microphone
-assign audio_clk = btn[3] ? ultrasonic_clk:standard_clk;
-
 // return curently used clock - so we can send it to microphone
-assign mic_clk_out = ~audio_clk;
+assign mic_clk_out = clk;
 
-wire neg_clk;
-assign neg_clk = audio_clk;
 reg [size-1:0] data;
-reg [size-1:0] smplcnt = 0;
+reg [size/8:0] smplcnt = 0;
 
 assign data_ready = &smplcnt;
 // switch case for different modes of microphone
 // for now we can just use slower clock and standard mode (2MHz)
 
-always @ (posedge audio_clk)
+always @ (posedge clk)
 	begin
 		// collect data when CLK is high
 		data <= {data[size-2:0], data_in};
 		smplcnt <= smplcnt + 1;
+	//	if(data_ready)
+			data_out <= data;
   end
 
-always @ (posedge neg_clk)
-	begin
-		// if we got all samples data send data
-		//if(smplcnt[size-1])
-		//	begin
-				data_out <= data;
-	 //		end
-	end
 endmodule
--- projects/I2S_RX/top.v
+++ projects/I2S_RX/top.v
@@ -3,7 +3,7 @@ module top (
     output [7:0] led,
     input [6:0] btn,
     input mic_data,
-	  output ftdi_rxd,
+    output ftdi_rxd,
     output mic_clk,
     output mic_select,
     output reg [3:0] audio_l,
@@ -26,25 +26,18 @@ pll_inst
 
 wire data_is_ready; // samples are ready
 
-reg [7:0] audio_data_out;
+reg [31:0] audio_data_out;
 reg [3:0] audio_data_buff_out;
 reg [31:0] cnt = 0;
 
 // If button 3 is pressed get data from buffer else just stream data from mic to headphones
-assign audio_l = btn[1] ? audio_data_buff_out:audio_data_out;
-assign audio_r = btn[1] ? audio_data_buff_out:audio_data_out;
-
-// If button 3 is pressed send 4MHz to microphone look in i2s_rx.v
-
-// read one sample before current
-reg [31:0] samplecount;
-wire buffer_clk;
+assign audio_l = btn[1] ? audio_data_buff_out:audio_data_out[3:0];
+assign audio_r = btn[1] ? audio_data_buff_out:audio_data_out[3:0];
 
 wire [31:0] raddr;
-assign raddr = samplecount;
+assign raddr = cnt;
 wire [31:0] waddr;
-assign waddr = samplecount;
-assign buffer_clk = cnt[2];
+assign waddr = cnt;
 
 databuff
 #(
@@ -52,40 +45,34 @@ databuff
   .size(4)
 )
 databuff_inst(
-  .clk(buffer_clk),             // needs to be double of data is ready
+  .clk(clk2MHz),             // needs to be double of data is ready
   .raddr(raddr),                // read samplecount
   .waddr(waddr),                // write samplecount
-  .we(data_is_ready),           //write only if data is ready
-  .datain(audio_data_out[7:4]),
+  .we(btn[2]),           
+  .datain(audio_data_out[3:0]),
   .dataout(audio_data_buff_out)
 );
 
 i2s_mic
 #(
   // How many samples we will collect before data ready
-  .size(8)
+  .size(32)
 )
 i2s_mic_inst(
-  .standard_clk(clk2MHz),
-  .ultrasonic_clk(clk4MHz),
-  .mic_clk_out(mic_clk),  //returns inverted clock that is used (standard/ultrasonic)
-  .btn(btn),
+  .clk(clk2MHz),
+  .mic_clk_out(mic_clk),  //returns clock 2MHz that is needed for microphone
   .data_in(mic_data),
   .data_ready(data_is_ready),
   .data_out(audio_data_out)
   );
 
-assign led[7] = cnt[2];
+assign led[7:0] = cnt[18:11];
 
-always @ (posedge mic_clk)
+always @ (posedge clk2MHz)
   begin
-    cnt <= cnt + 1;
-    if(data_is_ready) // when mic_clk counter is 4 and data_is_ready sample next sample is ready
+    if(data_is_ready) // if sample is ready
     begin
-      if (cnt[2])
-      begin
-        samplecount <= samplecount + 1;
-      end
+        cnt <= cnt + 1;
     end
   end
 
